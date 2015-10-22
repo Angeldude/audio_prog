6cc18a524641c4feb0ba3c4d2b255f4b935d9152
Angel Vanegas <angelman@gmail.com>
2015-10-22 09:18:17 -0400

    added pitch bend code

--- freq2midi.c
+++ freq2midi.c
@@ -8,18 +8,30 @@ int main()
   double c5;
   double fracmidi;
   double frequency;
-  int midinote;
+  double freq1;
+  double freq2;
+  int bend;
+  int midinote1;
+  int midinote2;
 
   semitone_ratio = pow(2.0, 1.0/12.0);
 
   c5 = 220.0 * pow(semitone_ratio, 3.0);
   c0 = c5 * pow(0.5, 5.0);
 
-  frequency = 400.0;
+  frequency = 460.0;
   fracmidi = log(frequency / c0) / log(semitone_ratio);
-  midinote = (int) (fracmidi + 0.5);
+  //calculating nearest midi notes +-
+  midinote1 = (int) (fracmidi + 0.5);
+  midinote2 = (int) (fracmidi - 0.5);
+  //calculating nearby frequencies
+  freq1 = c0 * pow(semitone_ratio, midinote1);
+  freq2 = c0 * pow(semitone_ratio, midinote2);
+  //calculating pitch bend of nearest midi note
+  bend = 100 *((frequency - freq1)/(freq1 - freq2));
 
 
-  printf("The nearest MIDI note to the frequency %f is %d\n", frequency, midinote);
+  printf("The nearest MIDI note to the frequency %f is %d\n", frequency, midinote1);
+  printf("with a pitchbend of %d%%\n", bend);
   return 0;
 }
