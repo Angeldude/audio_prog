3d34a37f031b37198faf96a0b58c3323a4dbcdbc
Angel Vanegas <angelman@gmail.com>
2015-10-29 10:28:54 -0400

    interactive midi2freq

--- midi2freq.c
+++ midi2freq.c
@@ -1,23 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 int main()
 {
-  double semitone_ratio;
-  double c0;
-  double c5;
-  double frequency;
+  double semitone_ratio, c0,c5,frequency;
   int midinote;
+  char message[256];
+  char* result;
 
   semitone_ratio = pow(2.0, 1.0/12.0);
-
   c5 = 220.0 * pow(semitone_ratio, 3.0);
-
   c0 = c5 * pow(0.5, 5.0);
 
-  midinote = 73;
+  printf("Enter MIDI note (0 - 127): ");
+  result = gets(message);
+  if(result == NULL){
+    printf("There was an error reading the input.\n");
+    return 1;
+  }
+  if(message[0] == '\0'){
+    printf("Have a nice day!\n");
+    return 1;
+  }
+  midinote = atoi(message);
+  if(midinote < 0){
+    printf("Sorry - %s is a bad MIDI note number.\n", message);
+    return 1;
+  }
+  if(midinote > 127){
+    printf("Sorry - %s is beyond the MIDI range!\n", message);
+    return 1;
+  }
   frequency = c0 * pow(semitone_ratio, midinote);
-
-  printf("MIDI Note %d has frequency %f\n", midinote, frequency);
+  printf("frequency of MIDI note %d = %f\n", midinote, frequency);
   return 0;
 }
